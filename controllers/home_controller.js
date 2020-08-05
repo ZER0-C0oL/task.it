437ef0266b7ca8b90920807bc73327dc4192805a
Mrudul Sankhere <32437824+ZER0-C0oL@users.noreply.github.com>
2020-08-05 18:46:13 +0530

    css stylings for home and add-task

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,8 @@
 var tasks = [
     { name: "task1", date: "3rd July 20", category: "Personal"},
     { name: "task2", date: "2nd July 20", category: "Work"},
-    { name: "task3", date: "4th August 20", category: "Other"}
+    { name: "task3", date: "4th August 20", category: "Other"},
+    { name: "task4", date: "11th June 20", category: "College"}
 ]
 
 module.exports.home = function(req, res) {
@@ -9,5 +10,11 @@ module.exports.home = function(req, res) {
         title: 'Home',
         project: 'task.it',
         task_list: tasks
+    });
+}
+
+module.exports.add_task = function(req, res) {
+    return res.render('add_task', {
+        title: "Add Task"
     });
 }
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const homeController = require('../controllers/home_controller');
 
+router.get('/add-task', homeController.add_task);
 router.get('/', homeController.home);
 
 module.exports = router
