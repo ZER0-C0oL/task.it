6fe4fe27b1dca6ca2e457ac11d6452b7267f47eb
Mrudul Sankhere <32437824+ZER0-C0oL@users.noreply.github.com>
2020-08-05 15:22:26 +0530

    home page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,13 @@
+var tasks = [
+    { name: "task1", date: "3rd July 20", category: "Personal"},
+    { name: "task2", date: "2nd July 20", category: "Work"},
+    { name: "task3", date: "4th August 20", category: "Other"}
+]
+
 module.exports.home = function(req, res) {
     return res.render('home',{
         title: 'Home',
-        project: 'task.it'
+        project: 'task.it',
+        task_list: tasks
     });
 }
--- index.js
+++ index.js
@@ -6,6 +6,8 @@ app.use('/', require('./routes'));
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
+app.use(express.urlencoded());
+app.use(express.static('assets'));
 
 
 app.listen(port, function(err){
